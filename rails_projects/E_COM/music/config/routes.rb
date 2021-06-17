f26ca17376e0505c277ff6d50a693068a0717157
aniladepu007 <ced16i002@iiitdm.ac.in>
2021-06-17 23:50:54 +0530

    pushing files

---
+++ rails_projects/E_COM/music/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+  get 'gallery/index'
+  get 'gallery/checkout'
+  post 'gallery/checkout'
+  get 'gallery/purchase_complete'
+  resources :orders
+  resources :line_items
+  resources :carts
+  get 'admin/login'
+  get 'admin/logout'
+  post 'admin/login'
+  resources :stores
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
